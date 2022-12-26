be04e6af7c6ddaf38db635fa85b5acf211d28299
Mohamed <77068169+mouaili@users.noreply.github.com>
2022-12-26 15:20:21 +0100

    Update app.js

--- Random-colors/app.js
+++ Random-colors/app.js
@@ -41,19 +41,19 @@ function addDeleteColors(e) {
 
   if (e.target.className === 'add-1') {
     allInputs[0].value = `#${randomColor.toUpperCase()}`;
-    allInputs[0].style.backgroundColor = `#${randomColor.toUpperCase()}`;
+    allInputs[0].style.backgroundColor = `#${randomColor}`;
     return;
   }
 
   if (e.target.className === 'add-2') {
     allInputs[1].value = `#${randomColor.toUpperCase()}`;
-    allInputs[1].style.backgroundColor = `#${randomColor.toUpperCase()}`;
+    allInputs[1].style.backgroundColor = `#${randomColor}`;
     return;
   }
 
   if (e.target.className === 'add-3') {
     allInputs[2].value = `#${randomColor.toUpperCase()}`;
-    allInputs[2].style.backgroundColor = `#${randomColor.toUpperCase()}`;
+    allInputs[2].style.backgroundColor = `#${randomColor}`;
     return;
   }
 
